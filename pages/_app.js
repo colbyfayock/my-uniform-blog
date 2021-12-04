b530d5e2327461daaad7a3cf8416416a1840f8cc
colbyfayock <fayock@gmail.com>
2021-12-03 20:58:43 -0500

    Step 7: setting up application to track for Uniform personalization

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { UniformTracker } from '@uniformdev/optimize-tracker-react';
+import { createDefaultTracker } from '@uniformdev/optimize-tracker-browser';
+import intentManifest from '../data/intent-manifest.json';
+
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const localTracker = createDefaultTracker({
+  intentManifest
+});
+
+function MyApp({ Component, pageProps, scoring }) {
+  return (
+    <UniformTracker trackerInstance={localTracker} initialIntentScores={scoring}>
+      <Component {...pageProps} />
+    </UniformTracker>
+  );
 }
 
 export default MyApp
